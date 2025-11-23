b6694ac2f90050ee485c1862ca0d555bef80a731
Mouad Oumous <mouadoumous@gmail.com>
2025-11-23 15:07:12 +0100

    accordian app

---
+++ src/components/accordian/index.jsx
@@ -0,0 +1,59 @@
+import data from "./data";
+import './styles.css';
+import { useState } from "react";
+
+// single selection
+
+export default function Accordian() {
+    const [selected, setSelected] = useState(null);
+    const [enableMultiSelection, setEnableMultiSelection] = useState(false);
+    const [multiple, setMultiple] = useState([]);
+
+    function handleSingleSelection(getCurrentId) {
+        setSelected(getCurrentId === selected ? null : getCurrentId);
+    }
+
+    function handleMultiSelection(getCurrentId) {
+        let cpyMultiple = [...multiple];
+        const findIndexCurrentId = cpyMultiple.indexOf(getCurrentId);
+
+        if(findIndexCurrentId === -1) cpyMultiple.push(getCurrentId);
+        else cpyMultiple.splice(findIndexCurrentId, 1);
+
+        setMultiple(cpyMultiple);
+    }
+    console.log(selected);
+    return <div className="wrapper">
+        <button 
+        onClick={() => setEnableMultiSelection(!enableMultiSelection)}>
+            Enable Multi Selection
+        </button>
+        <div className="accordian">
+            {
+                data && data.length > 0? 
+                data.map(dataItem=> <div className="item">
+                    <div onClick={
+                        enableMultiSelection 
+                        ? () => handleMultiSelection(dataItem.id)
+                        : () => handleSingleSelection(dataItem.id)
+                    } 
+                    className="title"
+                    >
+                        <h3>{dataItem.question}</h3>
+                        <span>+</span>
+                    </div> 
+                    {
+                        enableMultiSelection ? 
+                        multiple.indexOf(dataItem.id) !== -1 &&
+                        <div className="content"> {dataItem.answer} </div> :
+                        selected === dataItem.id && <div className="content"> {dataItem.answer} </div>
+                    }
+                    
+                </div>)
+                : (<div>No data found</div>)
+            }
+        </div>
+        </div>;
+}
+
+// multiple selection
