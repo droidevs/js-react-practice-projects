5ba0c26721658892d877d96da8147e0a119e7f77
Mouad Oumous <mouadoumous@gmail.com>
2025-12-03 20:43:37 +0100

    implamentation of a tic tac toe game

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import TabTest from './components/custom-tabs/tab-test'
 import ModalTest from './components/custom-model-popup/modal-test'
 import GithubProfileFinder from './components/github-profile-finder'
 import SearchAutcomplete from './components/search-autocomplete-with-api'
+import TicTacToe from './components/tic-tac-toe'
 
 function App () {
   return (
@@ -43,7 +44,9 @@ function App () {
 
       {/* <GithubProfileFinder/> */}
 
-      <SearchAutcomplete/>
+      {/* <SearchAutcomplete/> */}
+
+      <TicTacToe/>
 
     </div>
   )
